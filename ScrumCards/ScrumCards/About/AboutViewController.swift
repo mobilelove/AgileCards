f154389a64a88d2584537d7317078be4ede4ad87
Vimal Veerachamy <Vimal.Veerachamy@ampf.com>
2018-02-05 21:05:25 -0600

    removed commented line

--- ScrumCards/ScrumCards/About/AboutViewController.swift
+++ ScrumCards/ScrumCards/About/AboutViewController.swift
@@ -12,8 +12,7 @@ class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
